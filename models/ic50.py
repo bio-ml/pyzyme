375087c4d5407cd76982e44b8c9de1d6dd8a2c9c
iansind <ian.sindelar@gmail.com>
2023-01-25 21:01:48 -0600

    fixed variables and initial predictions

--- models/ic50.py
+++ models/ic50.py
@@ -7,21 +7,23 @@ y = np.array([0.9, 0.85, 0.88, 0.5, 0.7, .1, .2])
 
 
 # Use of a three parameter model here sets the min at zero.
-def formula(X, A, B, C):
-    return A / (1 + (X / C)**B)
+def formula(X, max_y, hill_coeff, ic50):
+    return max_y / (1 + (X / ic50)**hill_coeff)
 
 
 # The formula that will be used to determine IC50 from the derived parameters. 
-def formula_inverted(Y_mid, A, B, C):
-    return (A / Y_mid - 1)**(1/B) * C
+def formula_inverted(Y_mid, max_y, hill_coeff, ic50):
+    return (max_y / Y_mid - 1)**(1/hill_coeff) * ic50
 
 
 def RSS(init_params):
     predictions = formula(x, *init_params)
     return sum((y - predictions)**2)
 
-#TODO: create a method for determining these based on the data. E.g. max value can be the maximum observed value from x.
-initial_predictions = np.array([1, -1.5, 400])
+# Predicts likely initial parameters to minimize from.
+ip_max_y = max(y)
+ip_ic50 = np.median(x)
+initial_predictions = np.array([ip_max_y, -1.5, ip_ic50])
 
 # Using the scipy minimize function to optimize from initial predicted parameters. 
 result = minimize(RSS, initial_predictions, method='Powell')
@@ -37,8 +39,9 @@ R2 = 1.0 - (np.var(abs_error) / np.var(y))
 
 # Uses a very large number to find roughly the upper bound for Y. Can probably be improved mathematically. 
 y_mean = formula(1000000, *fitted_params) / 2
+ic50_from_plot = formula_inverted(y_mean, *fitted_params)
 
-ic50 = formula_inverted(y_mean, *fitted_params)
+ic50_calc = fitted_params[2]
 
-print(f'The IC50 occurs at {np.round(ic50, 2)} units of x. The R squared of the regression is {np.round(R2, 2)}.')
+print(f'The IC50 occurs at {np.round(ic50_calc, 2)} units of x. The R squared of the regression is {np.round(R2, 2)}.')
 
